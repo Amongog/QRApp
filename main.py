02812fb6ea309762cf6fbbae574ea307d5b6e75f
jberros <samuel.berrocal@ucr.ac.cr>
2021-11-15 18:46:13 -0600

    Archivo principal para la aplicación.

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------------------------
+#             IE-0117 Programación Bajo Plataformas Abiertas
+#                      Proyecto Python: App Código QR
+#         Grupo 06: Samuel Berrocal Soto. samuel.berrocal@ucr.ac.cr
+#                   Mario Benavides
+#                   David Campos Espinoza
+# -----------------------------------------------------------------------------
+import qrcode
+import cv2
